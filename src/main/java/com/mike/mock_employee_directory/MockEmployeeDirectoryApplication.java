7ca7e4db8f22bc7e607bb15753c6318c3b40b869
modusami <128193218+modusami@users.noreply.github.com>
2024-06-07 15:10:48 -0400

    initial files

---
+++ src/main/java/com/mike/mock_employee_directory/MockEmployeeDirectoryApplication.java
@@ -0,0 +1,13 @@
+package com.mike.mock_employee_directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockEmployeeDirectoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockEmployeeDirectoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mike/mock_employee_directory/MockEmployeeDirectoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mike.mock_employee_directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockEmployeeDirectoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
